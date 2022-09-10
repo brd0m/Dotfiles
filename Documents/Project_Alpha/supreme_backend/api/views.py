ad86d5b59de5bffb9b08e56158f15c8c8c8efd1e
BrDom <brentrusselldomingo@gmail.com>
2022-09-10 22:27:48 +0800

    Kitty

---
+++ Documents/Project_Alpha/supreme_backend/api/views.py
@@ -0,0 +1,40 @@
+from os import set_inheritable
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import TodoSerializer, TodoToggleCompleteSerializer
+from todo.models import Todo
+# Create your views here.
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+    #need to provide user who posted TODOs
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        # user can only update, delete own posts
+        return Todo.objects.filter(user=user)
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+    
+    def perform_update(self,serializer):
+        serializer.instance.completed = not(serializer.instance.completed)
+        serializer.save()
+ 
