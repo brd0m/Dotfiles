21cdc97b6a8de03ce517b37be36d7720847d53dd
brd0m <113246520+brd0m@users.noreply.github.com>
2022-09-11 01:01:36 +0800

    Delete Documents directory

--- Documents/Project_Alpha/supreme_backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('todos/', views.TodoListCreate.as_view()),
-    path('todos/<int:pk>',views.TodoRetrieveUpdateDestroy.as_view()),
-    path('todos/<int:pk>/complete',views.TodoToggleComplete.as_view()),
-]
